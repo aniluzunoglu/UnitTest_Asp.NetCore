144283487b6c62a5f96ae8af9eb1287698aadde3
Anıl Uzunoğlu <aniluzunoglu@outlook.com>
2021-09-02 23:41:52 +0300

    calculator sum equal unitTest

---
+++ XUnitTest.APP/UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.APP;
+using Xunit;
+
+namespace UnitTest.Test {
+    public class CalculatorTest {
+
+        [Fact]
+        public void SumTest() {
+            //Arrange
+
+            //değişkenleri initialize ettiğimiz yer. İlk değer vereceğim veya nesne örneği oluşturacağım yer
+
+            int a = 5;
+            int b = 20;
+            var calculator = new Calculator();
+
+            //Act
+
+            //initialize ettiğimiz classlara parametreler verip test edeceğimiz methodları çalıştırdığımız yer.
+
+            var total = calculator.sum(a, b);
+
+            //Assert 
+
+            //doğrulama evresidir. Act evresinden çıkan sonuç beklediğim mi değil mi burada test edilir.
+
+            Assert.Equal<int>(25, total);
+        }
+    }
+}
